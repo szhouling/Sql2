2b610c9feb3897c24ffffa1a08fb3eeeeeffea70
Zee Shen <150317462+szhouling@users.noreply.github.com>
2024-08-06 01:33:18 -0700

    Create  Code Testcase Testcase Test Result 626. Exchange Seats.sql

---
+++ Code Testcase Testcase Test Result 626. Exchange Seats.sql
@@ -0,0 +1,6 @@
+select case when id % 2 <> 0 and id = (select max(id) from Seat) then id
+            when id % 2 <> 0 then id + 1
+            else id - 1
+        end as id, student
+from Seat
+order by 1;
